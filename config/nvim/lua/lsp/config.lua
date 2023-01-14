85f72d492ef12d0f42c06c0c41a3452efae70838
Matthew Fisher <matt.fisher@fermyon.com>
2023-01-14 13:01:56 -0800

    update to latest neovim
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- config/nvim/lua/lsp/config.lua
+++ config/nvim/lua/lsp/config.lua
@@ -31,7 +31,7 @@ local on_attach = function(client, bufnr)
 	buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- C# (omnisharp)
 local pid = vim.fn.getpid()
